0974166f4132b855ed46b8195be9314da4cc09a2
ByJoker8625 <janick.otto@t-online.de>
2022-02-09 21:10:18 +0100

    Add Json.kt

---
+++ src/main/java/de/byjoker/myjfql/util/Json.kt
@@ -0,0 +1,35 @@
+package de.byjoker.myjfql.util
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import java.io.File
+
+object Json {
+
+    private val OBJECT_MAPPER = ObjectMapper()
+
+    fun stringify(any: Any): String {
+        return OBJECT_MAPPER.writeValueAsString(any)
+    }
+
+    fun read(file: File): JsonNode {
+        return OBJECT_MAPPER.readTree(file)
+    }
+
+    fun write(any: Any, file: File) {
+        OBJECT_MAPPER.writeValue(file, any);
+    }
+
+    fun parse(json: String): JsonNode {
+        return OBJECT_MAPPER.readTree(json)
+    }
+
+    fun <T> parse(json: String, clazz: Class<T>): T {
+        return this.parse(parse(json), clazz)
+    }
+
+    fun <T> parse(node: JsonNode, clazz: Class<T>): T {
+        return OBJECT_MAPPER.treeToValue(node, clazz)
+    }
+
+}
