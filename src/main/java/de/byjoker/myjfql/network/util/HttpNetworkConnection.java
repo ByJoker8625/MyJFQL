2ae6f45d69ad34ea7af7bd2e58ab4231b15616e1
ByJoker8625 <janick.otto@t-online.de>
2022-02-17 09:54:32 +0100

    Add support for trusted domains

--- src/main/java/de/byjoker/myjfql/network/util/HttpNetworkConnection.java
+++ src/main/java/de/byjoker/myjfql/network/util/HttpNetworkConnection.java
@@ -1,5 +1,6 @@
 package de.byjoker.myjfql.network.util;
 
+import de.byjoker.myjfql.core.MyJFQL;
 import de.byjoker.myjfql.exception.NetworkException;
 import de.byjoker.myjfql.network.session.Session;
 import de.byjoker.myjfql.util.Json;
@@ -9,6 +10,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 public class HttpNetworkConnection implements Connection {
 
@@ -38,7 +40,11 @@ public class HttpNetworkConnection implements Connection {
          * Only responding in PLAIN_TEXT instead of APPLICATION_JSON to support older connectors.
          */
 
-        context.contentType(ContentType.TEXT_PLAIN).result(Json.stringify(response).getBytes(StandardCharsets.UTF_8));
+
+        context.contentType(ContentType.TEXT_PLAIN)
+                .header("Access-Control-Allow-Origin", MyJFQL.getInstance().getConfig().getServer().getTrusted().stream().map(trusted -> trusted + " ").collect(Collectors.joining()))
+                .result(Json.stringify(response)
+                        .getBytes(StandardCharsets.UTF_8));
     }
 
     @Override
